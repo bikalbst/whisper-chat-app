cb3c8096b46a98ce524007323efc3aa2927d1953
bikalbst <bikalthakuri9809@gmail..com>
2025-11-23 20:01:48 +0545

    added signup controller

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    })
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true, //prevents XXS attacks: cross-site scripting
+        sameSite: "strict", //also prevents CSRF attacks: cross-site request forgery
+        secure: process.env.NODE_ENV === "development" ? false : true,
+        //cookie only sent over HTTPS in production
+
+    });
+    return token;
+}
